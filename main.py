01403046d9dd9a0e5cc8dbe102b7b90890256056
tomoyk <tmyk.kym@gmail.com>
2021-07-17 23:43:15 +0900

    fix type

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import hashlib
 
-def gen_hash(in_txt: str, salt: str):
+def gen_hash(in_txt: str, salt: str) -> bytes:
     b_txt = bytes(salt + in_txt, 'utf-8')
     m = hashlib.sha256(b_txt)
     return m.digest()[0]
 
-def calc(k, in_txt):
+def calc(k: int, in_txt: str):
     for i in range(k):
         yield gen_hash(in_txt, str(i))
 
@@ -13,8 +13,8 @@ def main():
     bfindex = 0  # long
     m = 64
     k = 3
-    for p in calc(k, "hello"):
-        fit_hash = p % m
+    for c in calc(k, "hello"):
+        fit_hash = c % m
         invol = 2 ** fit_hash
         print(invol)
         bfindex = bfindex | invol
